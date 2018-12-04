88b4707587c6311cabc28841f49266b3ae0cb7f1
cyriltso <cyril.tso2406@gmail.com>
2018-12-04 22:00:37 +0100

    group stages database added

--- test/test.py
+++ test/test.py
@@ -19,3 +19,15 @@ print(db)
 print(db2)
 print(db3)
 
+listtest = [['1', 'Arsenal', ' ', '6', '4', '2', '0', '18', '6', '+12', '14'], ['2', 'Paris', 'Saint-Germain', '6', '3', '3', '0', '13', '7', '+6', '12']]
+print(listtest)
+
+test = []
+
+for i, j in listtest:
+    if i % 2 == 1:
+        test.append('GROUPE A').append(j)
+    elif i % 2 == 0:
+        test.append('GROUPE B').append(j)
+
+print(test)
